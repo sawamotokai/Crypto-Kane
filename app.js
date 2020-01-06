d8c157251ea4ae81e9832949214699f44354be53
Kai Sawamoto <kaisawamoto@gmail.com>
2020-01-05 16:45:26 -0800

    addTransaction endpoint done

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const app = require('express')();
 const Kane = require('./models/Kane');
+const uuid4 = require('uuid4');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const kane = new Kane();
+const nodeAddress = uuid4().replace(/-/g, ''); // address of node on port 3000
 
 app.get('/', (req, res) => {
-	res.send(`${kane.findNonce(4)}`);
+	res.send(`Welcome to my cryptocurrency Kane! ${kane.findNonce(4)}`);
 });
 
 app.get('/mine_block', (req, res) => {
 	let prev_block = kane.lastBlock;
 	let new_nonce = kane.findNonce(prev_block.nonce);
+	kane.createTransaction(nodeAddress, 'Kai', 10);
 	let block = JSON.parse(kane.createBlock(new_nonce, kane.hash(prev_block)));
 	console.log(block);
 	res.status(200).json(block);
@@ -29,6 +35,13 @@ app.get('/is_valid_chain', (req, res) => {
 		message: isValid ? 'Good. Blockchain is valid' : 'Bad. Blockchain is invalid'
 	};
 	return res.status(200).json(respond);
+});
+
+app.post('/add_transaction', (req, res) => {
+	const { sender, receiver, amount } = req.body;
+	if (!sender || !receiver || !amount) return res.status(400).json({ message: 'Some param is missing.' });
+	let targetBlockIdx = kane.createTransaction(sender, receiver, aomunt);
+	return res.status(201).json({ message: `Transaction to be added to Block with index: ${targetBlockIdx}` });
 });
 
 const port = 3000;
